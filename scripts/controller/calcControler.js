b587a878a4d532230ffbb6c0e4a326040e8cd5d6
Ewerton Coutinho <53920901+ebcoutinho91@users.noreply.github.com>
2020-03-23 22:34:33 -0300

    Add files via upload

---
+++ scripts/controller/calcControler.js
@@ -0,0 +1,80 @@
+class CalcController{
+/* Se for usar um recurso mais de uma vez, transforma-lo em método
+ */
+    constructor(){
+
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector('#display')
+        this._dateEl = document.querySelector('#data')
+        this._timeEl = document.querySelector('#hora')
+        this._currentDate
+        this.initialize()
+    }
+
+    initialize(){
+
+        this.setDisplayDateTime()
+
+        setInterval(()=>{
+
+            this.setDisplayDateTime()
+
+        }, 1000)
+
+
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('#buttons > g, #parts >g')
+    }
+
+    /* Getters */
+
+    get displayTime(){
+
+        return this._timeEl.innerHTML
+    }
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML    
+    }
+
+    get displayCalc(){
+
+        return this._displayCalcEl.innerHTML
+    }
+
+    get currentDate(){
+
+        return new Date()
+    }
+
+    /* Setters */
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {day: '2-digit', month: 'long', year: 'numeric'})
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+        
+    }
+
+    set displayTime(value){
+
+        this._timeEl.innerHTML = value
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value
+    }
+
+    set currentDate(valaue){
+
+        this._currentDate.innerHTML = value
+    }
+
+}
